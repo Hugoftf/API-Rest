7d3b2336ed00d556144655c65d75fc0e671d23ec
Hugo de França Torres Ferreira <f3rreirahugo@hotmail.com>
2025-03-18 11:55:49 -0300

    adicionando imagens

--- src/main/java/com/github/Hugoftf/Spring/JPA/controller/dto/AutorDTO.java
+++ src/main/java/com/github/Hugoftf/Spring/JPA/controller/dto/AutorDTO.java
@@ -1,13 +1,18 @@
 package com.github.Hugoftf.Spring.JPA.controller.dto;
 
 import com.github.Hugoftf.Spring.JPA.model.Autor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorDTO( UUID id,
+                        @NotBlank(message = "Campo Obrigatório")
                         String nome,
+                       @NotNull(message = "Campo Obrigatório")
                        LocalDate dataNascimento,
+                       @NotBlank(message = "Campo Obrigatório")
                        String nacionalidade) {
 
 
